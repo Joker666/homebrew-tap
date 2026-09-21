7bf39ad3e82af76e3a43660785b93f814e97330b
Rafi <mr.k779@outlook.com>
2026-09-21 03:30:45 -0400

    Update Reflex to 0.2.1

--- Casks/reflex.rb
+++ Casks/reflex.rb
@@ -1,8 +1,8 @@
 cask "reflex" do
-  version "0.1.0"
-  sha256 "eebeab8670bd7b108b614a860f24d1b80be70501b891e2bc24625045a79fbd53"
+  version "0.2.1"
+  sha256 "ee3f030a7a464a0ae2d11e180a8f7ee5155ad09f00301b541b894eb24f545853"
 
-  url "https://github.com/Joker666/Reflex/releases/download/v#{version}/Reflex-#{version}-3.dmg"
+  url "https://github.com/Joker666/Reflex/releases/download/v#{version}/Reflex-#{version}-5.dmg"
   name "Reflex"
   desc "Route web links to the appropriate browser and profile"
   homepage "https://github.com/Joker666/Reflex"
