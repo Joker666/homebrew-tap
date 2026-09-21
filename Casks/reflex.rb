45fa0543b710ce3f7683436657e8297cb184d0c0
Rafi <mr.k779@outlook.com>
2026-09-20 21:28:25 -0400

    Match cask version to release tag

--- Casks/reflex.rb
+++ Casks/reflex.rb
@@ -1,8 +1,8 @@
 cask "reflex" do
-  version "0.1.0,3"
+  version "0.1.0"
   sha256 "eebeab8670bd7b108b614a860f24d1b80be70501b891e2bc24625045a79fbd53"
 
-  url "https://github.com/Joker666/Reflex/releases/download/v#{version.csv.first}/Reflex-#{version.csv.first}-#{version.csv.second}.dmg"
+  url "https://github.com/Joker666/Reflex/releases/download/v#{version}/Reflex-#{version}-3.dmg"
   name "Reflex"
   desc "Route web links to the appropriate browser and profile"
   homepage "https://github.com/Joker666/Reflex"
